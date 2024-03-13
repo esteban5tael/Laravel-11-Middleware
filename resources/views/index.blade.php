a04f9851ab61ff8a4f42192975abc73f0fe36b67
esteban5tael <esteban5tael@gmail.com>
2024-03-12 19:22:47 -0500

    Install Breeze

---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.base')
+
+@section('title')
+    {{ config('app.name') }}
+@endsection
+
+@section('styles')
+    {{-- <style></style> --}}
+@endsection
+
+@section('h3')
+    {{ config('app.name') }}
+@endsection
+
+@section('content')
+    <div class="card m-5">
+        <div class="card-header">
+            <div class="card-title text-center">
+                <h3>{{ config('app.name') }}</h3>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="card-text text-center">
+                {{ config('app.name') }}
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="card-text text-center">
+                {{ config('app.name') }}
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    {{-- <script></script> --}}
+@endsection
